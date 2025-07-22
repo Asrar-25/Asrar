8169c2c82093ec08c8145244aac9fcbd636dc9a6
Asrar-25 <asrarahmed@deloitte.com>
2025-07-22 23:18:37 +0530

    Homepage

--- src/test/java/org/pages/basePage.java
+++ src/test/java/org/pages/basePage.java
@@ -1,7 +1,16 @@
 package org.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.utils.driverManager;
+
+import java.time.Duration;
+
+import static org.utils.driverManager.driver;
 
 public class basePage {
      public driverManager d = new driverManager();
@@ -9,5 +18,12 @@ public class basePage {
         PageFactory.initElements(d.getDriver(),this);
 
     }
+
+    public void waits(WebElement w){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(300));
+        wait.until(ExpectedConditions.elementToBeClickable(w));
+
+    }
+
 
 }
--- src/test/java/org/pages/loginPage.java
+++ src/test/java/org/pages/loginPage.java
@@ -1,10 +1,11 @@
 package org.pages;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.pageObjects.loginPageObjects;
-
+import static org.utils.driverManager.driver;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,11 +13,72 @@ public class loginPage extends loginPageObjects {
 
 
     public void loginintoapplication() throws InterruptedException, IOException {
-        d.getDriver().get("https://www.skyscanner.co.in/");
-        Thread.sleep(10000);
+        d.getDriver().get("https://www.expedia.com/");
+        Thread.sleep(3000);
+        TakesScreenshot ts = (TakesScreenshot) d.getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File destination = new File(System.getProperty("user.dir")+"/screenshot.png");
+        FileUtils.copyFile(source,destination);
+
+    }
+
+    public void screenShot() throws IOException {
         TakesScreenshot ts = (TakesScreenshot) d.getDriver();
         File source = ts.getScreenshotAs(OutputType.FILE);
         File destination = new File(System.getProperty("user.dir")+"/screenshot.png");
         FileUtils.copyFile(source,destination);
     }
+
+
+    public void clickFlightsLink(){
+        linkFlights.click();
+        driver.findElement(By.xpath("/*[text()='Flights']//parent::a"));
+       // javaScriptExe(linkFlights);
+
+    }
+
+    public void oneWay() {
+        linkOneway.click();
+        //javaScriptExe(linkOneway);
+    }
+
+    public void leavingFrom(String city) {
+        buttonFrom.click();
+        inputFrom.sendKeys(city);
+        buttonCity.click();
+        //javaScriptExe(buttonFrom);
+        //inputFrom.sendKeys(city);
+        //javaScriptExe(buttonCity);
+
+    }
+
+    public void goingTo(String city) {
+        buttonTo.click();
+        inputTo.sendKeys(city);
+        buttonCityy.click();
+        //javaScriptExe(buttonTo);
+        //inputTo.sendKeys(city);
+        //javaScriptExe(buttonCityy);
+
+
+    }
+
+    public void travelers() {
+
+        buttonPassenger.click();
+        buttonAdult.click();
+        buttonDone.click();
+        //javaScriptExe(buttonPassenger);
+        //javaScriptExe(buttonAdult);
+        //javaScriptExe(buttonDone);
+
+
+    }
+
+    public void search() {
+        buttonSearch.click();
+        //javaScriptExe(buttonSearch);
+    }
+
+
 }
